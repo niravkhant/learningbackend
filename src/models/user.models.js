dfe5abd3e1bbb733b238b09139d1c0b56c202413
Nirav Khant <29600493+niravkhant@users.noreply.github.com>
2024-01-21 15:26:42 +0530

    add register controller

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,6 +1,7 @@
 import mongoose, {Schema} from "mongoose";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+import { upload } from "../middlewares/multer.middleware.js";
 
 
 
@@ -28,6 +29,7 @@ const userSchema = new Schema({
     },
     avatar:{
         type: String, // cloudinary url for image
+        required: true
     },
     coverImage:{
         type: String,
@@ -50,7 +52,7 @@ const userSchema = new Schema({
 userSchema.pre("save", async function(next){
     if(!this.isModified("password")) return next();
 
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 });
 
