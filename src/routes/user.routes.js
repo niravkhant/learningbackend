5e2b438d480085e7ef1431ed38291bde04be037b
Nirav Khant <29600493+niravkhant@users.noreply.github.com>
2024-01-23 00:28:15 +0530

    new chnages

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -23,5 +23,5 @@ router.route("/login").post(loginUser)
 // secured routes
 
 router.route("/logout").post(verifyJWT,logoutUser);
-router.route("refresh-token").post(refreshAccessToken)
+router.route("/refresh-token").post(refreshAccessToken);
 export default router;
